7178dcd077a56da06d53f7bff3339bc8dd16feb3
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-06-12 18:26:55 +0530

    Passing jokes from jokes api to voiceRSS

--- script.js
+++ script.js
@@ -111,10 +111,14 @@ const VoiceRSS = {
   },
 };
 
-function test() {
+// VoiceRSS speech function
+function tellMe(joke) {
+  console.log(joke);
+
+  //   VoiceRSS speech parameters
   VoiceRSS.speech({
     key: '8d92fb47dacd4411a2d6f25b632c2560',
-    src: 'Hello, world!',
+    src: joke,
     hl: 'en-us',
     v: 'Linda',
     r: 0,
@@ -124,4 +128,26 @@ function test() {
   });
 }
 
-test();
+// Get Jokes from Jokes Api
+async function getJokes() {
+  let joke = '';
+  const apiUrl =
+    'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist,explicit';
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    // Assign one part or two part joke
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // Passing jokes from jokes api to voiceRSS
+    tellMe(joke);
+  } catch (error) {
+    console.log('Error!', error);
+  }
+}
+
+getJokes();
